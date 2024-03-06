c25a928140526de6ea419e8b9edad52bf43b6043
buianhhuy96 <73339292+buianhhuy96@users.noreply.github.com>
2024-03-06 11:04:44 +0200

    Add targets for public builds (#13)
    - Add "updateAttrs" as custom update attributes and lists method, that allow to concatenate lists instead of overwriting them (like lib.recursiveUpdate)
    - Add public targets which has a "extend" that indicates the target that they are based on.
    - Reconfigure "config-processor-hardware" and "config-processor-installer" for new public targets
    
    Signed-off-by: Anh Huy Bui <buianhhuy96@gmail.com>

---
+++ installers/fmo-os-installer-public.nix
@@ -0,0 +1,37 @@
+# Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# FMO-OS general installer includes images for Rugged tablet and laptop without registration agent
+#
+{
+  # system and host description
+  sysconf = {
+    extend = "./fmo-os-installer.nix";
+    name = "fmo-os-installer-public";
+    extraModules = [
+      {
+        services = {
+          registration-agent-laptop ={
+            enable = false;
+          }; # services.registration-agent-laptop
+        }; # services
+      }
+    ]; # extraModules
+
+    installer = {
+      name = "pterm-installer";
+      enable = true;
+      run_on_boot = true;
+      welcome_msg = "Welcome to FMO-OS installer";
+      mount_path = "/home/ghaf/root";
+      custom_script_path = "";
+      custom_script_env_path = [];
+    }; # installer
+
+    # OS to include
+    oss = [
+      "fmo-os-rugged-laptop-7330-public"
+      "fmo-os-rugged-tablet-7230-public"
+    ]; # oss
+  }; # system
+}
