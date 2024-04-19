d39106a5982c01af99b9a2860aa7d548cd820345
buianhhuy96 <73339292+buianhhuy96@users.noreply.github.com>
2024-04-19 13:01:29 +0300

    Add custom QWERTY US layout for squeekboard (#33)
    - Create a new layout and overwrite the default layout via a patch file
    - Apply patch before building squeekboard
    - New layout is applied for all applications
    
    Signed-off-by: Anh Huy Bui <buianhhuy96@gmail.com>

--- modules/custom-packages/squeekboard/default.nix
+++ modules/custom-packages/squeekboard/default.nix
@@ -2,14 +2,21 @@
 # SPDX-License-Identifier: Apache-2.0
 (final: _prev: {
   squeekboard = _prev.squeekboard.overrideAttrs (oldAttrs: {
-        postInstall = ''
-            mkdir -p $out/share/dbus-1/services
-            cat <<END > $out/share/dbus-1/services/sm.puri.OSK0.service
-            [D-BUS Service]
-            Name=sm.puri.OSK0
-            Exec=$out/bin/squeekboard
-            END
-        '';
-     });
+    postUnpack = oldAttrs.postUnpack or "" + ''
+      cat $src/data/keyboards/terminal/us_wide.yaml
+    '';
+    prePatch = '' 
+      ls .
+      '';
+    patches = oldAttrs.patches or [] ++ [ ./us_wide.patch ];
+    postInstall = ''
+        mkdir -p $out/share/dbus-1/services
+        cat <<END > $out/share/dbus-1/services/sm.puri.OSK0.service
+        [D-BUS Service]
+        Name=sm.puri.OSK0
+        Exec=$out/bin/squeekboard
+        END
+    '';
+  });
   squeekboard-control = final.callPackage ./squeekboard.nix {pkgs=final;};
 })
