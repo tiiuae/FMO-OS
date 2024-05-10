99f3a70e26f71426e74935e33268f3dfae2be90e
buianhhuy96 <buianhhuy96@gmail.com>
2024-05-10 16:14:51 +0300

    Modularize services with flake-parts
    - Move and rename some modules matching ghaf mainline structure
    - Create separate folder for hydrajobs
    - Modularize with flake-parts
    - Rewrite target config with flake-part modules
    
    Signed-off-by: Anh Huy Bui <buianhhuy96@gmail.com>

---
+++ modules/fmo-services/default.nix
@@ -0,0 +1,15 @@
+# Copyright 2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# Services for FMO
+#
+{
+  imports = [
+    ./dci-service
+    ./hostname-service
+    ./portforwarding-service
+    ./psk-distribution-host
+    ./psk-distribution-vm
+    ./registration-agent-laptop
+  ];
+}
