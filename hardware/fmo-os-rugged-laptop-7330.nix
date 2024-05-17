f0f4939bc0776c90537cb59797151118b785ae54
Joonas Onatsu <joonas.onatsu@unikie.com>
2024-05-18 00:24:43 +0300

    Update Sway config
    -Add device-specific touch screen IDs
    -Reformat device config files
    
    Signed-off-by: Joonas Onatsu <joonasx@ssrc.tii.ae>

--- hardware/fmo-os-rugged-laptop-7330.nix
+++ hardware/fmo-os-rugged-laptop-7330.nix
@@ -29,11 +29,11 @@
     extraModules = [
       {
         # Add NVMe support into initrd to be able to boot from it
-        boot.initrd.availableKernelModules = [ "nvme" "ahci" ];
+        boot.initrd.availableKernelModules = ["nvme" "ahci"];
 
         services = {
           fmo-psk-distribution-service-host = {
-              enable = true;
+            enable = true;
           };
           registration-agent-laptop = {
             enable = true;
@@ -46,6 +46,13 @@
             '';
           }; # services.udev
         }; # services
+        ghaf.graphics.sway.swayConfig = {
+          input = {
+            "3823:49155:CUST0000:00_0EEF:C003" = {
+              map_to_output = "eDP-1";
+            };
+          };
+        }; # Sway touch screen
       }
     ]; # extraModules;
 
@@ -60,144 +67,145 @@
           "tcpdump"
         ]; # systemPackages
         extraModules = [
-        {
-          users.users."ghaf".extraGroups = ["networkmanager"];
-          networking = {
-            nat = {
-              enable = true;
-              internalIPs = [ "192.168.101.0/24" ];
-            }; # networking.nat
-            wireless = {
-              enable = true;
-            };
-            networkmanager = {
-              enable = true;
-              unmanaged = [
-                "ethint0"
-              ];
-            };
-          }; # networking
+          {
+            users.users."ghaf".extraGroups = ["networkmanager"];
+            networking = {
+              nat = {
+                enable = true;
+                internalIPs = ["192.168.101.0/24"];
+              }; # networking.nat
+              wireless = {
+                enable = true;
+              };
+              networkmanager = {
+                enable = true;
+                unmanaged = [
+                  "ethint0"
+                ];
+              };
+            }; # networking
 
-          services = {
-            udev = {
-              extraRules = ''
-                # Rename network devices
-                SUBSYSTEM=="net", ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idProduct}=="a4a2", ATTRS{idVendor}=="0525", NAME="mesh0"
-                SUBSYSTEM=="net", ACTION=="add", DRIVERS=="e1000e", SUBSYSTEMS=="pci", ATTRS{vendor}=="0x8086", NAME="eth0"
-              '';
-            }; # services.udev
+            services = {
+              udev = {
+                extraRules = ''
+                  # Rename network devices
+                  SUBSYSTEM=="net", ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idProduct}=="a4a2", ATTRS{idVendor}=="0525", NAME="mesh0"
+                  SUBSYSTEM=="net", ACTION=="add", DRIVERS=="e1000e", SUBSYSTEMS=="pci", ATTRS{vendor}=="0x8086", NAME="eth0"
+                '';
+              }; # services.udev
 
-            avahi = {
-              enable = true;
-              nssmdns = true;
-              reflector = true;
-            }; # services.avahi
+              avahi = {
+                enable = true;
+                nssmdns = true;
+                reflector = true;
+              }; # services.avahi
 
-            fmo-psk-distribution-service-vm = {
-              enable = true;
-            };
+              fmo-psk-distribution-service-vm = {
+                enable = true;
+              };
 
-            portforwarding-service = {
-              enable = true;
-              ipaddress = "192.168.100.12";
-              ipaddress-path = "/etc/NetworkManager/system-connections/ip-address";
-              configuration = [
+              portforwarding-service = {
+                enable = true;
+                ipaddress = "192.168.100.12";
+                ipaddress-path = "/etc/NetworkManager/system-connections/ip-address";
+                configuration = [
+                  {
+                    dip = "192.168.101.11";
+                    dport = "4222";
+                    sport = "4222";
+                    proto = "tcp";
+                  }
+                  {
+                    dip = "192.168.101.11";
+                    dport = "4222";
+                    sport = "4222";
+                    proto = "udp";
+                  }
+                  {
+                    dip = "192.168.101.11";
+                    dport = "7222";
+                    sport = "7222";
+                    proto = "tcp";
+                  }
+                  {
+                    dip = "192.168.101.11";
+                    dport = "7222";
+                    sport = "7222";
+                    proto = "udp";
+                  }
+                  {
+                    dip = "192.168.101.11";
+                    dport = "4223";
+                    sport = "4223";
+                    proto = "tcp";
+                  }
+                  {
+                    dip = "192.168.101.11";
+                    dport = "4223";
+                    sport = "4223";
+                    proto = "udp";
+                  }
+                ];
+              }; # services.portforwarding-service;
+            }; # services
+
+            microvm = {
+              devices = [
                 {
-                  dip = "192.168.101.11";
-                  dport = "4222";
-                  sport = "4222";
-                  proto = "tcp";
+                  bus = "pci";
+                  path = "0000:72:00.0";
                 }
                 {
-                  dip = "192.168.101.11";
-                  dport = "4222";
-                  sport = "4222";
-                  proto = "udp";
+                  bus = "pci";
+                  path = "0000:00:1f.0";
                 }
                 {
-                  dip = "192.168.101.11";
-                  dport = "7222";
-                  sport = "7222";
-                  proto = "tcp";
+                  bus = "pci";
+                  path = "0000:00:1f.3";
                 }
                 {
-                  dip = "192.168.101.11";
-                  dport = "7222";
-                  sport = "7222";
-                  proto = "udp";
+                  bus = "pci";
+                  path = "0000:00:1f.4";
                 }
                 {
-                  dip = "192.168.101.11";
-                  dport = "4223";
-                  sport = "4223";
-                  proto = "tcp";
+                  bus = "pci";
+                  path = "0000:00:1f.5";
                 }
                 {
-                  dip = "192.168.101.11";
-                  dport = "4223";
-                  sport = "4223";
-                  proto = "udp";
+                  bus = "pci";
+                  path = "0000:00:1f.6";
                 }
-              ];
-            }; # services.portforwarding-service;
-          }; # services
+              ]; # microvm.devices
 
-          microvm = {
-            devices = [
-              {
-                bus = "pci";
-                path = "0000:72:00.0";
-              }
-              {
-                bus = "pci";
-                path = "0000:00:1f.0";
-              }
-              {
-                bus = "pci";
-                path = "0000:00:1f.3";
-              }
-              {
-                bus = "pci";
-                path = "0000:00:1f.4";
-              }
-              {
-                bus = "pci";
-                path = "0000:00:1f.5";
-              }
-              {
-                bus = "pci";
-                path = "0000:00:1f.6";
-              }
-            ]; # microvm.devices
+              # WAR: Default microvm's way to passthrough usb devices is not working
+              # Lets use qemu.extraArgs for that
+              qemu.extraArgs = [
+                "-usb"
+                "-device"
+                "usb-host,vendorid=0x0525,productid=0xa4a2"
+              ]; # microvm.qemu.extraArgs
 
-            # WAR: Default microvm's way to passthrough usb devices is not working
-            # Lets use qemu.extraArgs for that
-            qemu.extraArgs = [
-              "-usb"
-              "-device"
-              "usb-host,vendorid=0x0525,productid=0xa4a2"
-            ]; # microvm.qemu.extraArgs
+              shares = [
+                {
+                  source = "/var/netvm/netconf";
+                  mountPoint = "/etc/NetworkManager/system-connections";
+                  tag = "netconf";
+                  proto = "virtiofs";
+                  socket = "netconf.sock";
+                }
+                {
+                  tag = "ssh-public-key";
+                  source = "/run/ssh-public-key";
+                  mountPoint = "/run/ssh-public-key";
+                }
+              ]; # microvm.shares
+            }; # microvm
 
-            shares = [
-              {
-                source = "/var/netvm/netconf";
-                mountPoint = "/etc/NetworkManager/system-connections";
-                tag = "netconf";
-                proto = "virtiofs";
-                socket = "netconf.sock";
-              }
-              {
-                tag = "ssh-public-key";
-                source = "/run/ssh-public-key";
-                mountPoint = "/run/ssh-public-key";
-              }
-            ]; # microvm.shares
-          }; # microvm
-
-          fileSystems."/run/ssh-public-key".options = ["ro"];
-          # For WLAN firmwares
-          hardware.enableRedistributableFirmware = true;
-        }]; # extraModules
+            fileSystems."/run/ssh-public-key".options = ["ro"];
+            # For WLAN firmwares
+            hardware.enableRedistributableFirmware = true;
+          }
+        ]; # extraModules
       }; # netvm
 
       dockervm = {
@@ -212,63 +220,66 @@
           "gpsd"
         ]; # systemPackages
         extraModules = [
-        {
-          users.users."ghaf".extraGroups = ["docker" "dialout"];
-          microvm = {
-            mem = 4096;
-            vcpu = 2;
-            # WAR: Default microvm's way to passthrough usb devices is not working
-            # Lets use qemu.extraArgs for that
-            qemu.extraArgs = [
-              "-usb"
-              "-device"
-              "usb-host,vendorid=0x1546,productid=0x01a9"
-            ]; # microvm.qemu.extraArgs
-            volumes = [{
-              image = "/var/tmp/dockervm.img";
-              mountPoint = "/var/lib/docker";
-              size = 51200;
-              autoCreate = true;
-              fsType = "ext4";
-            }];# microvm.volumes
-            shares = [
-              {
-                source = "/var/fogdata";
-                mountPoint = "/var/lib/fogdata";
-                tag = "fogdatafs";
-                proto = "virtiofs";
-                socket = "fogdata.sock";
-              }
-            ]; # microvm.shares
-          };# microvm
-          services = {
-            fmo-hostname-service = {
-              enable = true;
-              hostname-path = "/var/lib/fogdata/hostname";
-            }; # services.fmo-hostnam-service
-            fmo-dci = {
-              enable = true;
-              compose-path = "/var/lib/fogdata/docker-compose.yml";
-              pat-path = "/var/lib/fogdata/PAT.pat";
-              preloaded-images = "tii-offline-map-data-loader.tar.gz";
-            }; # services.fmo-dci
-            avahi = {
-              enable = true;
-              nssmdns = true;
-            }; # services.avahi
-            registration-agent-laptop = {
-              enable = true;
-              run_on_boot = true;
-              certs_path = "/var/lib/fogdata/certs";
-              config_path = "/var/lib/fogdata";
-              token_path = "/var/lib/fogdata";
-              hostname_path = "/var/lib/fogdata";
-              ip_path = "/var/lib/fogdata";
-              post_install_path = "/var/lib/fogdata/certs";
-            }; # services.registration-agent-laptop
-          }; # services
-          networking.firewall.enable = false;
-        }]; # extraModules
+          {
+            users.users."ghaf".extraGroups = ["docker" "dialout"];
+            microvm = {
+              mem = 4096;
+              vcpu = 2;
+              # WAR: Default microvm's way to passthrough usb devices is not working
+              # Lets use qemu.extraArgs for that
+              qemu.extraArgs = [
+                "-usb"
+                "-device"
+                "usb-host,vendorid=0x1546,productid=0x01a9"
+              ]; # microvm.qemu.extraArgs
+              volumes = [
+                {
+                  image = "/var/tmp/dockervm.img";
+                  mountPoint = "/var/lib/docker";
+                  size = 51200;
+                  autoCreate = true;
+                  fsType = "ext4";
+                }
+              ]; # microvm.volumes
+              shares = [
+                {
+                  source = "/var/fogdata";
+                  mountPoint = "/var/lib/fogdata";
+                  tag = "fogdatafs";
+                  proto = "virtiofs";
+                  socket = "fogdata.sock";
+                }
+              ]; # microvm.shares
+            }; # microvm
+            services = {
+              fmo-hostname-service = {
+                enable = true;
+                hostname-path = "/var/lib/fogdata/hostname";
+              }; # services.fmo-hostnam-service
+              fmo-dci = {
+                enable = true;
+                compose-path = "/var/lib/fogdata/docker-compose.yml";
+                pat-path = "/var/lib/fogdata/PAT.pat";
+                preloaded-images = "tii-offline-map-data-loader.tar.gz";
+              }; # services.fmo-dci
+              avahi = {
+                enable = true;
+                nssmdns = true;
+              }; # services.avahi
+              registration-agent-laptop = {
+                enable = true;
+                run_on_boot = true;
+                certs_path = "/var/lib/fogdata/certs";
+                config_path = "/var/lib/fogdata";
+                token_path = "/var/lib/fogdata";
+                hostname_path = "/var/lib/fogdata";
+                ip_path = "/var/lib/fogdata";
+                post_install_path = "/var/lib/fogdata/certs";
+              }; # services.registration-agent-laptop
+            }; # services
+            networking.firewall.enable = false;
+          }
+        ]; # extraModules
       }; # dockervm
     }; # vms
   }; # system
