c7365aebfeaebe06a30e069a7c90d380c73c90da
Ivan Kuznetsov <jsvapiav@gmail.com>
2024-06-04 04:34:02 +0000

    FMO-37: Need to introduce update by replacing files - Check if update docker-compose file exist - If so then backup original file - And update original docker-compose file with a new one
    Signed-off-by: Ivan Kuznetsov <jsvapiav@gmail.com>

--- modules/fmo-services/dci-service/default.nix
+++ modules/fmo-services/dci-service/default.nix
@@ -17,6 +17,14 @@ in {
       type = types.str;
       description = "Path to docker-compose's .yml file";
     };
+    update-path = mkOption {
+      type = types.str;
+      description = "Path to docker-compose's .yml file for update";
+    };
+    backup-path = mkOption {
+      type = types.str;
+      description = "Path to docker-compose's .yml file for backup";
+    };
     preloaded-images = mkOption {
       type = types.str;
       description = "Preloaded docker images file names separated by spaces";
@@ -39,9 +47,31 @@ in {
     script = ''
         USR=$(${pkgs.gawk}/bin/gawk '{print $1}' ${cfg.pat-path} || echo "")
         PAT=$(${pkgs.gawk}/bin/gawk '{print $2}' ${cfg.pat-path} || echo "")
-        DCPATH=$(echo ${cfg.compose-path} )
+        DCPATH=$(echo ${cfg.compose-path})
+        UPDPATH=$(echo ${cfg.update-path})
+        BCPPATH=$(echo ${cfg.backup-path})
         PRELOAD_PATH=$(echo ${preload_path})
         DOCKER_URL=$(echo ${cfg.docker-url})
+
+        # Check if the update file exists
+        if [ -e "$UPDPATH" ]; then
+            echo "Update file exists. Proceeding with backup and update operations"
+
+            # Backup the original file if it exists
+            if [ -e "$DCPATH" ]; then
+                echo "Backing up the original file"
+                mv "$DCPATH" "$BCPPATH"
+            else
+                echo "No original file to backup"
+            fi
+
+            # Move the new file to replace the original file
+            mv "$UPDPATH" "$DCPATH"
+            echo "Move completed successfully"
+        else
+            echo "Update file does not exist. No operations performed"
+        fi
+
         if [ -z "$DOCKER_URL" ]; then
           DOCKER_URL="cr.airoplatform.com"
         fi
