b5adab20e111030c4a25cfb0e87e4de941429d2f
Joonas Onatsu <joonas.onatsu@unikie.com>
2024-07-26 17:57:10 +0300

    Cleanup and refactor package module
    Signed-off-by: Joonas Onatsu <joonas.onatsu@unikie.com>

--- modules/packages/sway-input-config/sway-input-config.nix
+++ modules/packages/sway-input-config/sway-input-config.nix
@@ -1,43 +1,53 @@
 {
-  lib,
   stdenv,
-  xkeyboard_config,
-  python3Packages,
+  lib,
   fetchFromGitHub,
+  python3Packages,
+  xkeyboard_config,
 }:
-with python3Packages;
-  python3Packages.buildPythonPackage rec {
-    pname = "sway-input-config";
-    version = "1.4.2";
+python3Packages.buildPythonPackage rec {
+  pname = "sway-input-config";
+  version = "1.4.2";
 
-    src = fetchFromGitHub {
-      owner = "Sunderland93";
-      repo = pname;
-      rev = "v${version}";
-      sha256 = "sha256-By03kzT3ypJY5R/B1YMwf1poxvW5cqJe72jiR4x5RL4=";
-    };
+  src = fetchFromGitHub {
+    owner = "Sunderland93";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-By03kzT3ypJY5R/B1YMwf1poxvW5cqJe72jiR4x5RL4=";
+  };
 
-    format = "setuptools";
-    doCheck = false;
-    doInstallCheck = false;
+  #format = "setuptools";
 
-    propagatedBuildInputs =
-      [
-        xkeyboard_config
-      ]
-      ++ (with python3Packages; [setuptools pyqt6 i3ipc]);
+  # Non-Python deps for build and run-time
+  buildInputs = [
+    xkeyboard_config
+  ];
 
-    patches = [./support-only-linux.patch];
-    postPatch = ''
-      substituteInPlace sway_input_config/main.py \
-        --replace /usr ${xkeyboard_config}
-    '';
+  # Build-time only Python deps
+  build-system = with python3Packages; [
+    setuptools
+  ];
 
-    meta = with lib; {
-      description = "Input device configurator for Sway ";
-      homepage = "https://github.com/Sunderland93/sway-input-config";
-      license = licenses.gpl3;
-      platforms = platforms.linux;
-      maintainers = with maintainers; [sei40kr];
-    };
-  }
+  # Build and run-time Python deps
+  dependencies = with python3Packages; [
+    pyqt6
+    i3ipc
+  ];
+
+  # No tests defined
+  doCheck = false;
+  doInstallCheck = false;
+
+  patches = [./support-only-linux.patch];
+  postPatch = ''
+    substituteInPlace sway_input_config/main.py \
+      --replace /usr ${xkeyboard_config}
+  '';
+
+  meta = with lib; {
+    description = "Input device configurator for Sway ";
+    homepage = "https://github.com/Sunderland93/sway-input-config";
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+  };
+}
