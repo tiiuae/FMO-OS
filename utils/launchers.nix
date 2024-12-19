b79246e2e82619f367f8c86354c77ed9e6085253
Joonas Onatsu <joonas.onatsu@unikie.com>
2024-12-19 19:30:37 +0200

    Fix typos and reformat
    Signed-off-by: Joonas Onatsu <joonas.onatsu@unikie.com>

--- utils/launchers.nix
+++ utils/launchers.nix
@@ -1,31 +1,32 @@
 # Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{
-  sysconf,
-}: {
+{sysconf}: {
   lib,
   pkgs,
   ...
-}: with lib;
-let
-  get_launcer = descr: (
-  let
-    extraArgs = if lib.hasAttr "extraArgs" descr then descr.extraArgs else "";
-    launcers_description = {
-      weston-terminal = {
-       path = "${pkgs.weston}/bin/weston-terminal ${extraArgs}";
-       icon = "${pkgs.weston}/share/weston/icon_terminal.png";
-      };
+}:
+with lib; let
+  getLauncher = descr: (
+    let
+      extraArgs =
+        if lib.hasAttr "extraArgs" descr
+        then descr.extraArgs
+        else "";
+      launchers = {
+        weston-terminal = {
+          path = "${pkgs.weston}/bin/weston-terminal ${extraArgs}";
+          icon = "${pkgs.weston}/share/weston/icon_terminal.png";
+        };
 
-      chromium = {
-        path = "${pkgs.chromium}/bin/chromium --enable-features=UseOzonePlatform --ozone-platform=wayland ${extraArgs}";
-        icon = "${pkgs.chromium}/share/icons/hicolor/24x24/apps/chromium.png";
+        chromium = {
+          path = "${pkgs.chromium}/bin/chromium --enable-features=UseOzonePlatform --ozone-platform=wayland ${extraArgs}";
+          icon = "${pkgs.chromium}/share/icons/hicolor/24x24/apps/chromium.png";
+        };
       };
-    };
-  in
-    launcers_description."${descr.app}"
+    in
+      launchers."${descr.app}"
   );
 in {
   ghaf.graphics.weston.enableDemoApplications = lib.mkIf (lib.hasAttr "launchers" sysconf) (lib.mkForce false);
-  ghaf.graphics.weston.launchers = lib.mkIf (lib.hasAttr "launchers" sysconf) (map get_launcer sysconf.launchers);
+  ghaf.graphics.weston.launchers = lib.mkIf (lib.hasAttr "launchers" sysconf) (map getLauncher sysconf.launchers);
 }
