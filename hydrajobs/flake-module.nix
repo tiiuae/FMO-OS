b8e113c699f277d036418b770986169100f97747
Joonas Onatsu <joonas.onatsu@unikie.com>
2025-01-02 17:17:33 +0200

    Generate hydrajobs automatically
    Signed-off-by: Joonas Onatsu <joonas.onatsu@unikie.com>

--- hydrajobs/flake-module.nix
+++ hydrajobs/flake-module.nix
@@ -1,11 +1,27 @@
 # Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{self, ...}: 
-{  
-  flake.hydraJobs = {
-    fmo-os-installer-public-debug.x86_64-linux = self.packages.x86_64-linux.fmo-os-installer-public-debug;
-    fmo-os-installer-public-release.x86_64-linux = self.packages.x86_64-linux.fmo-os-installer-public-release;
-    fmo-os-rugged-devices-public-debug.x86_64-linux = self.packages.x86_64-linux.fmo-os-rugged-devices-public-debug;
-    fmo-os-rugged-devices-public-release.x86_64-linux = self.packages.x86_64-linux.fmo-os-rugged-devices-public-release;
-  };
+#
+# TODO: For more robust target collection, maybe add an attribute
+# to the package config, or some similar trickery instead of
+# just checking the package name.
+#
+# Reference: https://wiki.nixos.org/wiki/Flakes#Output_schema
+# hydraJobs."<attr>"."<system>" = derivation;
+{
+  self,
+  lib,
+  ...
+}: let
+  packageFilter = name: value: ((lib.hasInfix "public" name) && !(lib.hasInfix "compressed" name));
+in {
+  flake.hydraJobs =
+    lib.foldlAttrs
+    (
+      acc: system: packages: let
+        publicPackages = lib.filterAttrs packageFilter packages;
+      in
+        acc // (lib.mapAttrs' (name: package: lib.nameValuePair name {${system} = package;}) publicPackages)
+    )
+    {}
+    self.packages;
 }
